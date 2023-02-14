b06a4a90075115be6f8f8371dc9adc2c691c84ef
Thanon_UP <thanon.sspk@gmail.com>
2023-02-15 01:18:49 +0700

    feat : install headiesui , added font , header text.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Canvas } from "@react-three/fiber";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+      <div id="header-container">
+        <h1 id="header-name">Portfolio</h1>
+        <h1 id="header-button">Portfolio</h1>
+      </div>
     </div>
+    // <div id="canvas-container">
+    //   <Canvas>
+    //     <ambientLight intensity={0.1} />
+    //     <directionalLight color="red" position={[0, 0, 5]} />
+    //     <mesh>
+    //       <boxGeometry />
+    //       <meshStandardMaterial />
+    //     </mesh>
+    //   </Canvas>
+    // </div>
   );
 }
 
